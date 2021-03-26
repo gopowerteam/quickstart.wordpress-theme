b7c8e02f91b273e7fc14fea3f5415e3985349311
zhuchentong <2037630@163.com>
2021-03-26 11:41:03 +0800

    update

---
+++ wordpress-theme/functions.php
@@ -0,0 +1,109 @@
+<?php
+// Helpers here serve as example. Change to suit your needs.
+
+// For a real-world example check here:
+// https://github.com/wp-bond/bond/blob/master/src/Tooling/Vite.php
+// https://github.com/wp-bond/boilerplate/tree/master/app/themes/boilerplate
+
+// on the links above there is also example for @vitejs/plugin-legacy
+
+
+// Some dev/prod mechanism would exist in your project
+// Handling manualy here, change to test both cases
+function vite($entry): string
+{
+    return jsTag($entry)
+        . jsPreloadImports($entry)
+        . cssTag($entry);
+}
+
+
+// Helpers to print tags
+function jsTag(string $entry): string
+{
+    $url =  assetUrl($entry);
+
+    return '<script type="module" crossorigin src="'
+        . $url
+        . '"></script>';
+}
+
+function jsPreloadImports(string $entry): string
+{
+
+
+    $res = '';
+    foreach (importsUrls($entry) as $url) {
+        $res .= '<link rel="modulepreload" href="'
+            . $url
+            . '">';
+    }
+    return $res;
+}
+
+function cssTag(string $entry): string
+{
+
+    $tags = '';
+    foreach (cssUrls($entry) as $url) {
+        $tags .= '<link rel="stylesheet" href="'
+            . $url
+            . '">';
+    }
+    return $tags;
+}
+
+
+/**
+ * 获取Manifest文件
+ */
+function getManifest(): array
+{
+    $content = file_get_contents(__DIR__ . '/manifest.json');
+
+    return json_decode($content, true);
+}
+
+/**
+ * 获取资源地址
+ */
+function assetUrl(string $entry): string
+{
+    $manifest = getManifest();
+  
+    return isset($manifest[$entry])
+        ? '/' . $manifest[$entry]['file']
+        : '';
+}
+
+/**
+ * 导入url地址
+ */
+function importsUrls(string $entry): array
+{
+    $urls = [];
+    $manifest = getManifest();
+
+    if (!empty($manifest[$entry]['imports'])) {
+        foreach ($manifest[$entry]['imports'] as $imports) {
+            $urls[] = '/' . $manifest[$imports]['file'];
+        }
+    }
+    return $urls;
+}
+
+/**
+ * 获取css地址
+ */
+function cssUrls(string $entry): array
+{
+    $urls = [];
+    $manifest = getManifest();
+
+    if (!empty($manifest[$entry]['css'])) {
+        foreach ($manifest[$entry]['css'] as $file) {
+            $urls[] = '/' . $file;
+        }
+    }
+    return $urls;
+}
---
+++ wordpress-theme/index.php
@@ -0,0 +1,26 @@
+  
+<?php
+// This would be your framework default bootstrap file
+
+// During dev, this file would be hit when accessing your local host, like:
+// http://vite-php-setup.test
+
+require_once __DIR__ . '/functions.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vite App</title>
+
+    <?= vite('index.html') ?>
+</head>
+
+<body>
+    <div id="app"></div>
+</body>
+
+</html>
