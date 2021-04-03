7ae230db112e7e656160549708f1f6796f1440fe
zhuchentong <2037630@163.com>
2021-04-03 16:05:04 +0800

    update

--- src/pages/loan.vue
+++ src/pages/loan.vue
@@ -3,31 +3,38 @@
 .loan
     .top-image 
         img.h-width(:src="LoadImage")
-    .product-container
+    .product-container(v-if="dataSource")
         .product-category.text-center.p-10(v-for="(category,index) in dataSource" :style="{ backgroundColor: index % 2 == 1 ? '#f7f7f7' : '#fff' }")
             .title.primary-color {{ category.label }}
             .subTitle.text-sm {{ category?.connectedNode?.node.description }}
             .children-container.flex.flex-row.justify-center.pt-3
-                .left-arrow
+                .left-arrow.pt-10(@click="pageIndex[index] -= 1" :style="{ visibility: pageIndex[index] > 0 ? 'visible' : 'hidden' }")
+                    img(:src="LeftIcon")
                 .content.flex.flex-row
-                    .product-item.p-2(v-for="product in category.children")
+                    .product-item.p-2(v-for="product in category?.children?.slice(0+pageIndex[index]*4,4+pageIndex[index]*4)")
                         img(:src="product?.connectedNode?.node?.featuredImage?.node?.mediaItemUrl")
-                        .title.text-xs.w-24.py-2 {{product.label}}
-                .right-arrow
+                        .title.text-xs.w-24.py-2 {{ product.label }}
+                .right-arrow.pt-10(@click="pageIndex[index] += 1" :style="{ visibility: (pageIndex[index] * 4 + 4) < category?.children?.length ? 'visible' : 'hidden' }")
+                    img(:src="RightIcon")
 </template>
 <script setup lang="ts">
 import { onMounted, watch } from "@vue/runtime-core";
+import { ref } from "vue";
 import LoadImage from '../assets/loan/loan-image.jpg'
-
+import LeftIcon from '../assets/loan/left.png'
+import RightIcon from '../assets/loan/right.png'
 
 const menus = JSON.parse(localStorage.getItem('menus') as string)
 const dataSource: any[] = menus.find(x => x.label === '贷款业务').children
 
+const pageIndex = ref(Array.from(Array(dataSource.length), () => 0))
 
+function onChangeProduct(index) {
 
+}
 
 onMounted(() => {
-    console.log(dataSource)
+    console.log(pageIndex)
 })
 </script>
 
@@ -38,6 +45,12 @@ onMounted(() => {
     color #F7B200
 .product-container
     .children-container
+        .left-arrow,.right-arrow
+            img 
+                height 40px
+        .content
+            width 480px
+            min-height 180px
         .product-item
             img
                 width 100px
